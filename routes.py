93fce79f515efdff03b5f62459338e1b0d9a6785
Tereza Drskova <tereza.drskova13@imperial.ac.uk>
2014-07-30 16:39:36 +0100

    before chaining controllers

--- routes.py
+++ routes.py
@@ -1,45 +1,43 @@
 from flask import Flask,jsonify
 import json
+from twython import Twython, TwythonError
+
+consumer_key = "RHuv5VHWqMEtd0vThJAlAwdQU"
+consumer_secret = "cCmcuxFQ0lGENvuOzCTKQa51rpnQdJbI281RwxMoVsj2PSAL9R"
+
+access_token_key = "2684988325-L9XobRukSFR29mthcZVPEqEw5Bm0YyohMotzedq"
+access_token_secret = "pBxT3rPk1fUqaXDtzZopd3olqqd4NEUF9C4QpCOQs9prW"
+
+# Requires Authentication as of Twitter API v1.1
+twitter = Twython(consumer_key, consumer_secret,access_token_key, access_token_secret)
 
 
 app = Flask(__name__)
 
-"""
-@menus.route('/get', methods=["PUT", "POST"])
-def new():
-    return jsonify(request.get_json(force=True))
-"""
 
 @app.route("/")
 def home():
 	return app.send_static_file('home.html')
 
-@app.route("/twitter")
-def twitter():
-	from twython import Twython, TwythonError
-
-	consumer_key = "RHuv5VHWqMEtd0vThJAlAwdQU"
-	consumer_secret = "cCmcuxFQ0lGENvuOzCTKQa51rpnQdJbI281RwxMoVsj2PSAL9R"
-
-	access_token_key = "2684988325-L9XobRukSFR29mthcZVPEqEw5Bm0YyohMotzedq"
-	access_token_secret = "pBxT3rPk1fUqaXDtzZopd3olqqd4NEUF9C4QpCOQs9prW"
-
-
-	# Requires Authentication as of Twitter API v1.1
-	twitter = Twython(consumer_key, consumer_secret,access_token_key, access_token_secret)
+@app.route("/timeline")
+def timeline():	
 	try:
 	    user_timeline = twitter.get_user_timeline(screen_name='timberners_lee')
 	except TwythonError as e:
 	    print e
 
+	return jsonify(**twitter.show_user(screen_name='timberners_lee'))
 
-	#twitterans = twitter.search(q='twitter', result_type='popular')
-	#print twitterans
 
-	#print user_timeline
+@app.route("/tweets")
+def tweets():	
+	try:
+		tweets = get_home_timeline(screen_name='timberners_lee')
+	except TwythonError as e:
+   		print e
 
-	return jsonify(**twitter.show_user(screen_name='drskovatereza'))
-	
+	return jsonify(**get_home_timeline(screen_name='timberners_lee'))
+
 
 if __name__ == "__main__":
 	app.run()

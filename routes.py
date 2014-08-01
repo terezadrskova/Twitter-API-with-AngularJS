bca84e1dafd686ead85a5e01d2858f87153b20b6
Tereza Drskova <tereza.drskova13@imperial.ac.uk>
2014-08-01 16:14:58 +0100

    before formatting tweets correctly

--- routes.py
+++ routes.py
@@ -8,9 +8,10 @@ consumer_secret = "cCmcuxFQ0lGENvuOzCTKQa51rpnQdJbI281RwxMoVsj2PSAL9R"
 access_token_key = "2684988325-L9XobRukSFR29mthcZVPEqEw5Bm0YyohMotzedq"
 access_token_secret = "pBxT3rPk1fUqaXDtzZopd3olqqd4NEUF9C4QpCOQs9prW"
 
+
 # Requires Authentication as of Twitter API v1.1
 twitter = Twython(consumer_key, consumer_secret,access_token_key, access_token_secret)
-
+username = 'timberners_lee'
 
 app = Flask(__name__)
 
@@ -22,22 +23,35 @@ def home():
 @app.route("/timeline")
 def timeline():	
 	try:
-	    user_timeline = twitter.show_user(screen_name='timberners_lee')
+		twitter.show_user(screen_name=username)
+		print twitter.show_user(screen_name=username)
 	except TwythonError as e:
 	    print e
 
-	return jsonify(**twitter.show_user(screen_name='timberners_lee'))
+	return jsonify(**twitter.show_user(screen_name=username))
 
 
 
 @app.route("/tweets")
 def tweets():	
 	try:
-	    user_timeline = twitter.show_user(screen_name='timberners_lee')
+		twitter.show_user(screen_name=username)
 	except TwythonError as e:
 	    print e
 
-	return jsonify(**twitter.show_user(screen_name='timberners_lee'))
+	user_tweets = twitter.get_user_timeline(screen_name='mikehelmick', include_rts=True)
+	tweetsObject = {}
+	tweetsArray = []
+	i = 0
+	for tweet in user_tweets:
+		tweet['text'] = Twython.html_for_tweet(tweet)
+		tweetsArray.append(tweet['text'])
+		tweetsObject[i] = tweet['text']
+		i += 1
+	tweet_obj = {'tweet_array': tweetsArray}
+	tweetsObject = json.dumps(tweetsObject)
+	return tweetsObject
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug = True)
